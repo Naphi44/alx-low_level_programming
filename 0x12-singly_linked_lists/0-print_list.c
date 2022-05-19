bd9a05fd9259637935272fe48e06a9cbe39e83c5
Naphi44 <bfikru0@gmail.com>
2022-05-19 13:26:30 -0700

    asdfg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,20 @@
 
 size_t print_list(const list_t *h)
 {
-register int len = 0;
+	register int len = 0;
 
-while (h)
-{
-if (!(h->str))
-{
-printf("[0] (nil)\n");
-}
-else
-{
-printf("[%i] %s\n", h -> len, h -> str);
-}
-h = h -> next;
-len++;
-}
-return (len);
+	while (h)
+	{
+		if (!(h->str))
+		{
+			printf("[0] (nil)\n");
+		}	
+		else
+		{
+			printf("[%i] %s\n", h -> len, h -> str);
+		}
+	h = h -> next;	
+	len++;
+	}
+	return (len);
 }
